5833ee0a7b83c701c584d5af05a135249695e1ad
xingchensong <xingchensong1996@163.com>
2023-08-21 19:41:36 +0800

    fix(tn&itn): fix a

--- itn/chinese/rules/postprocessor.py
+++ itn/chinese/rules/postprocessor.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2022 Zhendong Peng (pzd17@tsinghua.org.cn)
 # Copyright (c) 2023 Xingchen Song (sxc19@mails.tsinghua.edu.cn)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
