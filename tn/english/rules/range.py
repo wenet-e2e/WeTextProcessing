900f4fb91c056ef2df626a62a35bef19d26edea3
xingchensong <xingchensong1996@163.com>
2024-06-05 17:09:44 +0800

    [tn] english tn, support range

--- tn/english/rules/range.py
+++ tn/english/rules/range.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2024, WENET COMMUNITY.  Xingchen Song (sxc19@tsinghua.org.cn).
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
