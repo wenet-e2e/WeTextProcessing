c347f64b70c05ae068ee544eab599b349a91ec6b
pengzhendong <275331498@qq.com>
2026-06-10 10:34:32 +0800

    fix: use StringTokenType for OpenFst 1.8+ compatibility (#359)

--- runtime/processor/wetext_processor.cc
+++ runtime/processor/wetext_processor.cc
@@ -14,15 +14,15 @@
 
 #include "processor/wetext_processor.h"
 
-using fst::TokenType;
+using fst::StringTokenType;
 
 namespace wetext {
 Processor::Processor(const std::string& tagger_path,
                      const std::string& verbalizer_path) {
   tagger_.reset(StdVectorFst::Read(tagger_path));
   verbalizer_.reset(StdVectorFst::Read(verbalizer_path));
-  compiler_ = std::make_shared<StringCompiler<StdArc>>(TokenType::BYTE);
-  printer_ = std::make_shared<StringPrinter<StdArc>>(TokenType::BYTE);
+  compiler_ = std::make_shared<StringCompiler<StdArc>>(StringTokenType::BYTE);
+  printer_ = std::make_shared<StringPrinter<StdArc>>(StringTokenType::BYTE);
 
   if (tagger_path.find("zh_tn_") != tagger_path.npos) {
     parse_type_ = ParseType::kZH_TN;
